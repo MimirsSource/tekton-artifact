aa98bc8ab1b8de217833e505804a2e971d456068
quarkusio <no-reply@quarkus.io>
2021-08-10 07:01:20 +0000

    Initial commit

---
+++ src/test/java/de/tekton/artifact/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package de.tekton.artifact;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
